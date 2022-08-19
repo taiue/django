fef96234f5d9c9379c3857a8d7fa3f4dd7fe1fc7
taiue <80531794+taiue@users.noreply.github.com>
2022-08-19 10:33:18 -0300

    adicionando templates para home, about

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2022'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Blg home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
